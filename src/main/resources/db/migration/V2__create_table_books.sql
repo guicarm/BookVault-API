f5ef440eae8635af72d2ac84cd63bca645dc942e
guicarm <RM98874@fiap.com.br>
2024-09-23 10:56:37 -0300

    fix: collum name

--- src/main/resources/db/migration/V2__create_table_books.sql
+++ src/main/resources/db/migration/V2__create_table_books.sql
@@ -1,38 +1,41 @@
 CREATE TABLE t_bv_books (
     id bigint auto_increment,
     title varchar(255),
-    releaseYear INT,
+    releaseyear INT,
     genre varchar(255),
     primary key (id)
 
 );
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('Dom Casmurro', 1899, 'Romance');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('Cem Anos de Solidão', 1967, 'Realismo Mágico');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('Vidas Secas', 1938, 'Ficção Social');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('Ensaio sobre a Cegueira', 1995, 'Ficção');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('Gabriela, Cravo e Canela', 1958, 'Romance');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('A Cidade e os Cachorros', 1963, 'Romance');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('A Hora da Estrela', 1977, 'Romance');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('O Livro do Desassossego', 1982, 'Prosa Poética');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('O Tempo e o Vento', 1949, 'Romance Histórico');
 
-INSERT INTO t_bv_books (title, releaseYear, genre) 
+INSERT INTO t_bv_books (title, releaseyear, genre) 
 VALUES ('Memórias Póstumas de Brás Cubas', 1881, 'Romance');
+
+INSERT INTO t_bv_books (title, releaseyear, genre) 
+VALUES ('Baleia', 2000, 'Terror');
